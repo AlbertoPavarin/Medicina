1b7cf240fcf1ff137a2abe514bdc646d7aaae19f
AlbertoPavarin <albertopavarin@libero.it>
2023-03-21 13:13:28 +0100

    cassa

--- header.php
+++ header.php
@@ -1,4 +1,10 @@
 <?php
+include_once dirname(__FILE__) . '/functions/getUser.php';
+
+session_start();
+error_reporting(0);
+
+$user = getUser($_SESSION["user_id"]);
 
 ?>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
@@ -12,15 +18,29 @@
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="index.php?page=1">Attivita Didattiche</a>
         </li>
+        <?php 
+        if ($user["ruolo"] == "Admin")
+        {
+        ?>
         <li class="nav-item">
           <a class="nav-link active" href="index.php?page=2">Aggiungi Attività</a>
         </li>
+        <?php
+        }
+        ?>
         <li class="nav-item">
           <a class="nav-link active" href="index.php?page=3">Unità Didattiche</a>
         </li>
+        <?php 
+        if ($user["ruolo"] == "Admin")
+        {
+        ?>
         <li class="nav-item">
           <a class="nav-link active" href="index.php?page=4">Aggiungi Unità</a>
         </li>
+        <?php
+        }
+        ?>
       </ul>
     </div>
   </div>
